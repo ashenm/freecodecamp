17b898f873f7ff1fd2b772a8789c07ae043d1924
Ashen Gunaratne <mail@ashenm.ml>
2017-10-07 11:08:24 +0530

    Add completions

---
+++ front-end/basic/algorithm-scripting/repeat-a-string-repeat-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * Repeat a string repeat a string
+ * Repeat a given string n times 
+ *
+ * Ashen Gunaratne
+ * mail@ashenm.ml
+ *
+ */
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  return (num > 0) ? str.repeat(num) : '';
+}
